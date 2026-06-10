042493a61564f1c1951c6351fd73be1874404197
ntfs-sys <ntfs-sys@MDF-XX>
2026-06-11 03:21:28 +0400

    build.bash

--- pract/02/build.sh
+++ pract/02/build.sh
@@ -4,14 +4,14 @@ if [ ! -e build-linux ]; then
 	mkdir build-linux
 fi
 if [ ! -e build-linux/std.o ]; then
-	g++ -std=c++23 -fmodules -fsearch-include-path -c bits/std.cc -o build-linux/std.o 2>&1 | cowsay -f tux
+	g++ -std=c++23 -fmodules -fsearch-include-path -c bits/std.cc -o build-linux/std.o
 fi
 
 read -p "что билдим?" FNAME
 if [ ! -e "$FNAME.cpp" ]; then
 	echo "no."
 else
-	g++ -std=c++23 -fmodules "$FNAME.cpp" build-linux/std.o -o build-linux/$FNAME 2>&1 | cowsay -f tux
+	g++ -std=c++23 -fmodules "$FNAME.cpp" build-linux/std.o -o build-linux/$FNAME 
 	if [ $? -eq 0  ]; then
 		"./build-linux/$FNAME"
 	fi
