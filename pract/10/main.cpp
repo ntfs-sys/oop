e80227ab9bc55ee10df2ff0217cc8cc47f421fbc
ntfs-sys <ntfs-sys@MDF-XX>
2026-06-11 03:17:14 +0400

    asds

--- pract/10/main.cpp
+++ pract/10/main.cpp
@@ -4,7 +4,7 @@ import std;
 std::string readTextFromFile(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
-        return "HE YDAJOCb OTKPЫTЬ ФАЙЛ input.txt";
+        return "cant open input.txt";
     }
     
     std::string content;
